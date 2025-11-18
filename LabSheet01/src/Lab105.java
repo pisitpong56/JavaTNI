6cc75bcea4fc24c14f40aa282c612f04cc66128d
pisitpong56 <pisitpong242550@gmail.com>
2025-11-18 14:13:19 +0700

    Pisitpong LabSheet01

---
+++ LabSheet01/src/Lab105.java
@@ -0,0 +1,13 @@
+import javax.swing.JOptionPane;
+public class Lab105 {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,
+                "Summation number =" + (2+5) +
+                "\nSubtraction number =" + (2-5) +
+                "\nMultiplication number =" + (2*5) +
+                "\nDivision number =" + (2/5) +
+                "\nModulus number =" + (2%5),
+                "Arithmetic Operators",
+                JOptionPane.WARNING_MESSAGE);
+    }
+}
